d0869440fb0b3aa8b5748c1093283c841d3bd01c
kenwin728 <kenwin728@gmail.com>
2023-06-24 01:48:02 +0800

    added "make a post" feature

--- main.js
+++ main.js
@@ -9,3 +9,9 @@ function showReply(){
     var replyArea = document.getElementById("reply-area");
     replyArea.classList.remove("hide");
 }
+
+//Post
+function showPost(){
+    var postArea = document.getElementById("post-area");
+    postArea.classList.remove("hide");
+}
