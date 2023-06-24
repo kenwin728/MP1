14ba74d8f9ff481865e39dc5e9d22e27a9e8993c
Joshua Balcueva <129082897+Moonlit3406@users.noreply.github.com>
2023-06-25 00:34:23 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 //Comment
 function showComment(){
     var commentArea = document.getElementById("comment-area");
-    commentArea.classList.remove("hide");
+    commentArea.style.display = "block"
 }
 
+function removeComment(){
+    var commentArea = document.getElementById("comment-area");
+    commentArea.style.display = "none"
+}
 //Reply
 function showReply(){
     var replyArea = document.getElementById("reply-area");
-    replyArea.classList.remove("hide");
+    replyArea.style.display = "block"
+}
+    
+function removeReply(){
+    var commentArea = document.getElementById("reply-area");
+    commentArea.style.display = "none"
 }
 
 //Post
