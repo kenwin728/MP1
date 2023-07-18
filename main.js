d17eaba543e2a96d01e49962967737a9ee0b7a27
Hosea Marcus Calalang <hoseagb@gmail.com>
2023-07-19 03:38:00 +0800

    Changed Upvote and Downvote to like and Dislike

--- main.js
+++ main.js
@@ -29,3 +29,28 @@ function removePost(){
     let postArea = document.getElementById("post-area");
     postArea.style.display = "none";
 }
+
+let likebtn = document.querySelector('#likebtn');
+let dislikebtn = document.querySelector('#dislikebtn');
+let likebtn2 = document.querySelector('#likebtn2');
+let dislikebtn2 = document.querySelector('#dislikebtn2');
+let input1 = document.querySelector('#input1');
+let input2 = document.querySelector('#input2');
+let input1_com = document.querySelector('#input1_com');
+let input2_com = document.querySelector('#input2_com');
+
+likebtn.addEventListener('click', ()=>{
+    input1.value = parseInt(input1.value) + 1;
+})
+
+dislikebtn.addEventListener('click', ()=>{
+    input2.value = parseInt(input2.value) + 1;
+})
+
+likebtn2.addEventListener('click', ()=>{
+    input1_com.value = parseInt(input1_com.value) + 1;
+})
+
+dislikebtn2.addEventListener('click', ()=>{
+    input2_com.value = parseInt(input2_com.value) + 1;
+})
