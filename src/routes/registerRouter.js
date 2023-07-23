ecb24ea09c6c153ab440054de99fcbc8fcc9784c
kenwin728 <kenwin728@gmail.com>
2023-07-24 00:55:46 +0800

    password and confirm password must be the same to register

--- src/routes/registerRouter.js
+++ src/routes/registerRouter.js
@@ -19,16 +19,23 @@ registerRouter.post("/register", async (req, res) => {
         const user = await users.findOne({username: req.body.username});
         if (user){
             alert("Username already used!");
+            res.redirect("/register");
         }
         else{
-            const result = await users.insertOne({
-                username: req.body.username, 
-                password: req.body.password,
-                description: "",
-                photo: "",
-                numberofposts: 0
-            });
-            res.redirect("/login");
+            if (req.body.password === req.body.confirmpassword){
+                const result = await users.insertOne({
+                    username: req.body.username, 
+                    password: req.body.password,
+                    description: "",
+                    photo: "",
+                    numberofposts: 0
+                });
+                res.redirect("/login");
+            }
+            else{
+                alert("Password and Confirm Password don't match!");
+                res.redirect("/register");
+            }
         }
     } catch (err) {
         console.error(err);
