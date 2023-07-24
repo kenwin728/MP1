622e31c8955c4bbe339105063881f0b880bf60dc
kenwin728 <kenwin728@gmail.com>
2023-07-24 17:42:23 +0800

    Make a post button and cancel button can't show at the same time

--- public/js/main.js
+++ public/js/main.js
@@ -23,11 +23,19 @@ function removeReply(){
 function showPost(){
     var postArea = document.getElementById("post-area");
     postArea.style.display = "block";
+    var cancelButton = document.getElementById("cancel-btn");
+    cancelButton.style.display = "block";
+    var postButton = document.getElementById("post-btn");
+    postButton.style.display = "none";
 }
 
 function removePost(){
     let postArea = document.getElementById("post-area");
     postArea.style.display = "none";
+    var postButton = document.getElementById("post-btn");
+    postButton.style.display = "block";
+    var cancelButton = document.getElementById("cancel-btn");
+    cancelButton.style.display = "none";
 }
 
 let likebtn = document.querySelector('#likebtn');
