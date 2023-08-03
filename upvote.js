abf9b66d7bc05e7ea14393d5dce9f9911a939f52
kenwin728 <kenwin728@gmail.com>
2023-08-03 22:32:06 +0800

    Deleted phase 1 files

--- upvote.js
+++
@@ -1,9 +0,0 @@
-const upvoteButton = document.getElementById("upvoteButton");
-const voteCount = document.getElementById("voteCount");
-
-let count = 0;
-
-upvoteButton.addEventListener("click", () => {
-  count++;
-  voteCount.textContent = count;
-});
