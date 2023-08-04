ef4aa63b8e5ff023973c6185a7145f87d73f1eb1
kenwin728 <kenwin728@gmail.com>
2023-08-05 00:56:26 +0800

    Populate DB

--- index.js
+++ index.js
@@ -13,10 +13,13 @@ import bodyParser from "body-parser";
 import { mergeArrays } from './src/helpers/customHelpers.js';
 import handlebars from 'handlebars';
 import handlebarsHelpers from 'handlebars-helpers';
+import { getDb } from './src/db/conn.js';
+import bcrypt from 'bcrypt';
 
 async function main () {
     const __dirname = dirname(fileURLToPath(import.meta.url)); // directory URL
     const app = express();
+    const SALT_WORK_FACTOR = 10;
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use("/static", express.static(__dirname + "/public"));
     // Set handlebars as the express app's default view engine
@@ -46,6 +49,136 @@ async function main () {
             }
             console.log("Connected to Mongodb");
         });
+        const db = getDb();
+        const users = db.collection("users");
+        const posts = db.collection("posts");
+        const replies = db.collection("replies");
+        async function populateDB(){
+            const insertFiveUsers = await users.insertMany([{
+                username: "Josh", 
+                password: await bcrypt.hash("josh123", await bcrypt.genSalt(SALT_WORK_FACTOR)),
+                description: "",
+                photo: "",
+                numberofposts: 1
+            },{
+                username: "Kenwin", 
+                password: await bcrypt.hash("kenwin123", await bcrypt.genSalt(SALT_WORK_FACTOR)),
+                description: "",
+                photo: "",
+                numberofposts: 1
+            },{
+                username: "Marcus", 
+                password: await bcrypt.hash("marcus123", await bcrypt.genSalt(SALT_WORK_FACTOR)),
+                description: "",
+                photo: "",
+                numberofposts: 1
+            },{
+                username: "Johnson", 
+                password: await bcrypt.hash("johnson123", await bcrypt.genSalt(SALT_WORK_FACTOR)),
+                description: "",
+                photo: "",
+                numberofposts: 1
+            },{
+                username: "Bob", 
+                password: await bcrypt.hash("bob123", await bcrypt.genSalt(SALT_WORK_FACTOR)),
+                description: "",
+                photo: "",
+                numberofposts: 1
+            }]);
+            const insertFivePosts = await posts.insertMany([{
+                username: "Josh", 
+                title: "Is CCAPDEV hard?",
+                content: "Is CCAPDEV really hard?",
+                date: "08/5/2023",
+                postID: 1,
+                downvote: 0,
+                upvote: 0,
+                views: 0,
+                replies: 1
+            },{
+                username: "Kenwin", 
+                title: "Is STALGCM hard?",
+                content: "Is STALGCM really hard?",
+                date: "08/5/2023",
+                postID: 2,
+                downvote: 0,
+                upvote: 0,
+                views: 0,
+                replies: 1
+            },{
+                username: "Marcus", 
+                title: "Is ST-MATH hard?",
+                content: "Is ST-MATH really hard?",
+                date: "08/5/2023",
+                postID: 3,
+                downvote: 0,
+                upvote: 0,
+                views: 0,
+                replies: 1
+            },{
+                username: "Johnson", 
+                title: "Is GELITPH hard?",
+                content: "Is GELITPH really hard?",
+                date: "08/5/2023",
+                postID: 4,
+                downvote: 0,
+                upvote: 0,
+                views: 0,
+                replies: 1
+            },{
+                username: "Bob", 
+                title: "Is LCFAITH hard?",
+                content: "Is LCFAITH really hard?",
+                date: "08/5/2023",
+                postID: 5,
+                downvote: 0,
+                upvote: 0,
+                views: 0,
+                replies: 1
+            }]);
+            const insertFiveReplies = await replies.insertMany([{
+                username: "Josh", 
+                content: "Yes",
+                date: "08/5/2023",
+                postID: 2,
+                downvote: 0,
+                upvote: 0,
+                replyID: 1
+            },{
+                username: "Kenwin", 
+                content: "No",
+                date: "08/5/2023",
+                postID: 3,
+                downvote: 0,
+                upvote: 0,
+                replyID: 2
+            },{
+                username: "Marcus", 
+                content: "Somehow",
+                date: "08/5/2023",
+                postID: 4,
+                downvote: 0,
+                upvote: 0,
+                replyID: 3
+            },{
+                username: "Johnson", 
+                content: "Not really",
+                date: "08/5/2023",
+                postID: 5,
+                downvote: 0,
+                upvote: 0,
+                replyID: 4
+            },{
+                username: "Bob", 
+                content: "Maybe",
+                date: "08/5/2023",
+                postID: 1,
+                downvote: 0,
+                upvote: 0,
+                replyID: 5
+            }]);
+        }
+        populateDB();
     });
 }
 
