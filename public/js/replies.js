dc6f85504dd94a13a360afd7094acbb80f2555d8
Joshua Balcueva <129082897+Moonlit3406@users.noreply.github.com>
2023-08-06 17:16:41 +0800

    Added Show Hide Functions for edits post and comment

--- public/js/replies.js
+++ public/js/replies.js
@@ -16,3 +16,41 @@ function removePost(){
     var cancelButton = document.getElementById("cancel-btn");
     cancelButton.style.display = "none";
 }
+
+//Edit Post
+function showEdit(){
+    var editArea = document.getElementById("edit-area");
+    editArea.style.display = "block";
+    var cancelButton = document.getElementById("canceledit-btn");
+    cancelButton.style.display = "block";
+    var editButton = document.getElementById("edit-btn");
+    editButton.style.display = "none";
+}
+
+function removeEdit(){
+    let postArea = document.getElementById("edit-area");
+    postArea.style.display = "none";
+    var postButton = document.getElementById("edit-btn");
+    postButton.style.display = "block";
+    var cancelButton = document.getElementById("canceledit-btn");
+    cancelButton.style.display = "none";
+}
+
+//Edit User Comment
+function showEditComment(){
+    var editArea = document.getElementById("editcomment-area");
+    editArea.style.display = "block";
+    var cancelButton = document.getElementById("canceleditcomment-btn");
+    cancelButton.style.display = "block";
+    var editButton = document.getElementById("editcomment-btn");
+    editButton.style.display = "none";
+}
+
+function removeEditComment(){
+    let postArea = document.getElementById("editcomment-area");
+    postArea.style.display = "none";
+    var postButton = document.getElementById("editcomment-btn");
+    postButton.style.display = "block";
+    var cancelButton = document.getElementById("canceleditcomment-btn");
+    cancelButton.style.display = "none";
+}
